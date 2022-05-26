94e33fef8ff8eafbf08bf42a628bb346a8aa6c27
Feridun Mert Celebi <mertcelebi93@gmail.com>
2022-05-26 15:42:38 -0400

    Add basic error handling to filtered streams

--- src/filteredStream.ts
+++ src/filteredStream.ts
@@ -1,8 +1,36 @@
 import { TwitterAPI, ETwitterStreamEvent } from "./twitter";
 const twitter = new TwitterAPI();
+const stream = twitter.getStream();
+
+const gracefulExit = (err: Error) => {
+  console.error(err);
+  stream.close();
+};
+
+process.on("SIGTERM", gracefulExit);
+process.on("SIGINT", gracefulExit);
+process.on("unhandledRejection", gracefulExit);
 
 const streamTweets = async () => {
-  const stream = twitter.getStream();
+  // const allRules = await twitter.getAllStreamRules();
+  // console.log(allRules);
+  // await twitter.deleteRulesFromStream(allRules.data.map((rule) => rule.id));
+
+  stream.on(ETwitterStreamEvent.ConnectionError, (err) =>
+    console.error("Connection error: ", err)
+  );
+
+  stream.on(ETwitterStreamEvent.ConnectionClosed, () =>
+    console.log("Connection has been closed.")
+  );
+
+  stream.on(ETwitterStreamEvent.Connected, () =>
+    console.log("Connection has been established.")
+  );
+
+  stream.on(ETwitterStreamEvent.DataKeepAlive, () =>
+    console.log("Twitter heartbeat.")
+  );
 
   stream.on(ETwitterStreamEvent.Data, async (eventData) => {
     const { data, matching_rules } = eventData;
@@ -22,22 +50,6 @@ const streamTweets = async () => {
       }
     }
   });
-
-  stream.on(ETwitterStreamEvent.ConnectionError, (err) =>
-    console.error("Connection error: ", err)
-  );
-
-  stream.on(ETwitterStreamEvent.ConnectionClosed, () =>
-    console.log("Connection has been closed.")
-  );
-
-  stream.on(ETwitterStreamEvent.Connected, () =>
-    console.log("Connection has been established.")
-  );
-
-  stream.on(ETwitterStreamEvent.DataKeepAlive, () =>
-    console.log("Twitter heartbeat.")
-  );
 
   // Start stream!
   await stream.connect({
